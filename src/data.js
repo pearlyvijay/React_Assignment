166576d550b350718bfc15798c16ce6a38d164f3
pearlyvijay <98146563+pearlyvijay@users.noreply.github.com>
2022-07-11 09:24:57 -0700

    last commit

---
+++ src/data.js
@@ -0,0 +1,44 @@
+import img from "./Images/download (6).jpg";
+
+export const data = [
+	{
+		id: 1,
+		photo: img,
+		name: "Nord Stage 3 88 Keys",
+		music: "E.M.L.A Music",
+		price: 54545,
+		localtion: "Le Havre France",
+	},
+	{
+		id: 2,
+		photo: img,
+		name: "Line 6 Helix Rack Guitar Processor -",
+		music: "Pro Music",
+		price: 54545,
+		localtion: "Munich Germany",
+	},
+	{
+		id: 3,
+		photo: img,
+		name: "Nord Stage 3 88 Keys",
+		music: "E.M.L.A Music",
+		price: 54545,
+		localtion: "Le Havre France",
+	},
+	{
+		id: 4,
+		photo: img,
+		name: "Nord Stage 3 88 Keys",
+		music: "E.M.L.A Music",
+		price: 54545,
+		localtion: "Le Havre France",
+	},
+	{
+		id: 5,
+		photo: img,
+		name: "Nord Stage 3 88 Keys",
+		music: "E.M.L.A Music",
+		price: 54545,
+		localtion: "Le Havre France",
+	},
+];
